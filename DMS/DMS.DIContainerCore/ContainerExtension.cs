125420cc6e46e2c1aad99e4e526d804bf9b4100a
amir zouari <a.zouari@sds-ch.com>
2026-07-01 17:44:02 +0100

    commit

--- DMS/DMS.DIContainerCore/ContainerExtension.cs
+++ DMS/DMS.DIContainerCore/ContainerExtension.cs
@@ -39,6 +39,11 @@ namespace DMS.DIContainerCore
             services.AddTransient<IActivityRepository<TnActivite>, ActivityRepository>();
             services.AddTransient<IActivityService<ActivityDto>,
                 ActivityService<ActivityDto, TnActivite, DmsReferenceContext>>();
+
+            // GedDocumentCategory
+            services.AddTransient<IGedDocumentCategoryRepository<GedDocumentCategory>, GedDocumentCategoryRepository>();
+            services.AddTransient<IGedDocumentCategoryService<GedDocumentCategoryDto>,
+                GedDocumentCategoryService<GedDocumentCategoryDto, GedDocumentCategory, DmsReferenceContext>>();
         }
     }
 }

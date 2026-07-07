70a5a06274b0f6638a140958097dcea61ce96191
emna <khouloudbh@DESKTOP-JBOHVNI>
2026-07-07 11:43:30 +0100

    commit add custome fields to type file

---
+++ DMS/DMS.DTO/DTOs/CustomFieldDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.DTO.DTOs
+{
+    public class CustomFieldDto
+    {
+        public string CodeComplement { get; set; }
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool IsRequiredOnFileClosure { get; set; }
+    }
+}
---
+++ DMS/DMS.Infrastructure/IServices/ICustomFieldService.cs
@@ -0,0 +1,16 @@
+﻿using DMS.DTO.DTOs;
+using Master.Common.Interfaces.Services;
+
+namespace DMS.Infrastructure.IServices
+{
+    public interface ICustomFieldService<TEntityDTO> : IBaseService<TEntityDTO> where TEntityDTO : CustomFieldDto
+    {
+        Task<List<CustomFieldDto>> GetAllAsync();
+        Task<List<CustomFieldDto>> GetByFileTypeAsync(string codeTypeDossier);
+        List<string> GetAvailableTypes();
+        Task CreateAsync(CreateCustomFieldDto dto);
+        Task LinkAsync(LinkCustomFieldDto dto);
+        Task UnlinkAsync(string codeTypeDossier, string complementCode);
+        Task<bool> UpdateIsRequiredAsync(string codeTypeDossier, string complementCode, bool isRequiredOnFileClosure);
+    }
+}
